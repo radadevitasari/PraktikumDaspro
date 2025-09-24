2261791a56cc1af9389e109770cee1fd5eb8ac16
radadevitasari <radadevitasari87.com>
2025-09-24 20:09:33 +0700

    nambah github.java

---
+++ github.java
@@ -0,0 +1,8 @@
+public class github {
+    
+public static void main(String[] args) {
+    System.out.println("Nama saya rada dari Sib 1E");
+    System.err.println("Hai, ini baris kode yang di modifikasi Faradiza");
+}
+
+}
