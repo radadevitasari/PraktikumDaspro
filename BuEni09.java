f2d8eeb6b17f7c336c270e7f812aa94b096315ea
faradizalovy <lovyfaradiza@gmail.com>
2025-09-24 09:40:16 +0700

    first commit

---
+++ BuEni09.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BuEni09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        int uangSPP;
+        int uangGedung;
+        int lamaAngsuran;
+        double jumlahAngsuran;
+        double besaranUang;
+
+    System.out.println(" Masukkan uang SPP: ");
+    uangSPP = input.nextInt();
+
+    System.out.println("Masukkan uang Gedung: ");
+    uangGedung = input.nextInt();
+
+    System.out.println("Masukkan jumlah angsuran: ");
+    jumlahAngsuran = input.nextDouble();
+
+    System.out.println("Masukkan lama angsuran: ");
+    lamaAngsuran = input.nextInt();
+
+    besaranUang = uangSPP + uangGedung;
+
+    System.out.println(" Besaran uang = " +besaranUang);
+
+    }
+    
+}
