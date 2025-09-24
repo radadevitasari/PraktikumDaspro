13f28a53cd6cc8da50d95eff097a40dfc29bf7f0
faradizalovy <lovyfaradiza@gmail.com>
2025-09-24 18:53:12 +0700

    update tes.java oleh Faradiza

--- Halo.java
+++ Halo.java
@@ -1,6 +1,7 @@
 public class Halo {
 public static void main(String[] args) {
     System.out.println("Nama saya rada dari Sib 1E");
+    System.err.println("Hai, ini baris kode yang di modifikasi Faradiza");
 }
 
 }
