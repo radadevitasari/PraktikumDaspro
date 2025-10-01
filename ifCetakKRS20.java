4e4fe3e68d5ee563a72427349871b041492b3f35
radadevitasari <radadevitasari87.com>
2025-10-01 11:43:48 +0700

    percobaan1

---
+++ ifCetakKRS20.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ifCetakKRS20 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Cetak KRS SIAKAD");
+    System.out.println("Apakah UKT sudah lunas? (true/false)");
+    boolean uktlunas = sc.nextBoolean();
+    if (uktlunas) {
+        System.out.println("Pembayaran UKT terverifikasi");
+        System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        
+        
+    }
+}
+}
