f889f472c868106e0464db039f41f55143218ecb
radadevitasari <radadevitasari87.com>
2025-10-08 17:48:57 +0700

    input nama dan nim

---
+++ jobsheet6/CaseMethod1.java
@@ -0,0 +1,16 @@
+package PraktikumDaspro.jobsheet6;
+
+import java.util.Scanner;
+
+public class CaseMethod1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== INPUT DATA MAHASISWA ===");
+        System.out.print("Nama : ");
+        String nama = input.nextLine();
+        System.out.print("NIM  : ");
+        String nim = input.nextLine();
+
+    }
+}
