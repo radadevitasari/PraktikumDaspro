22e73355ec477e3847e3fc976d0ffe6cb3cb43d9
radadevitasari <radadevitasari87.com>
2025-10-08 18:05:03 +0700

    mata kuliah 1

--- jobsheet6/CaseMethod1.java
+++ jobsheet6/CaseMethod1.java
@@ -12,5 +12,11 @@ public class CaseMethod1 {
         System.out.print("NIM  : ");
         String nim = input.nextLine();
 
+        System.out.println("\nMata Kuliah 1: Algoritma & Pemrograman");
+        System.out.print("Nilai UTS: "); double uts1 = input.nextDouble();
+        System.out.print("Nilai UAS: "); double uas1 = input.nextDouble();
+        System.out.print("Nilai Tugas: "); double tugas1 = input.nextDouble();
+        double akhir1 = (uts1*0.3) + (uas1*0.4) + (tugas1*0.3);
+
     }
 }
